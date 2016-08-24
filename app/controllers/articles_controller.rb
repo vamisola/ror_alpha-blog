353a0885bf1bbbaaecdaf54470b471b864a3a710
Vina Compson <vv.compson@icloud.com>
2016-08-24 03:13:08 +0000

    add partials and destory action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,12 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted."
+        redirect_to articles_path
+    end
     
     def update
         @article = Article.find(params[:id])
