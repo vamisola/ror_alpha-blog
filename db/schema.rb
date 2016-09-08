94610e851b602e889b5f47c20818bb5c60a3db87
Vina Compson <vv.compson@icloud.com>
2016-09-08 03:54:35 +0000

    adding password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908025729) do
+ActiveRecord::Schema.define(version: 20160908035047) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
